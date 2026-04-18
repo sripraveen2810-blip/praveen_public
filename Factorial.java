4a0e30e2c377945cd6dfdb67f17c5f815b0947fd
praveen <sripraveen2810@gmail.com>
2026-04-18 17:03:43 +0530

    Add recursive factorial Java program

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+/*
+ * Name: praveen
+ * Email: sripraveen2810@gmail.com
+ */
+public class Factorial {
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is undefined for negative numbers.");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Factorial <non-negative integer>");
+            return;
+        }
+
+        int number = Integer.parseInt(args[0]);
+        System.out.println("Factorial of " + number + " is " + factorial(number));
+    }
+}
